c75a5a072e64c7b0ddfd22c22ab4f0fe14cf3f90
Nikolai <inikolaikovalenko@yandex.ru>
2018-05-10 13:04:14 +0300

    added the Dao package

--- src/main/java/ru/store/dao/Dao.java
+++ src/main/java/ru/store/dao/Dao.java
@@ -1,4 +1,10 @@
 package ru.store.dao;
 
-public class Dao {
+public interface Dao<Entity, Key> {
+
+    void create(Entity entity);
+    void delete(Entity entity);
+    void update(Entity entity);
+    Entity read (Key key);
+
 }
